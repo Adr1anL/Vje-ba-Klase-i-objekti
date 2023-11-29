0047db3c5d351167f4512041392db88e6b95c56e
Adr1anL <146196415+Adr1anL@users.noreply.github.com>
2023-11-29 09:29:21 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp6
             Zrakoplov PC21 = new Zrakoplov("Pilatus", 1200, 1333);
 
             Console.WriteLine(x380.ToString());
-            Console.WriteLine(PC21.ToString());
+            Console.WriteLine(C162.ToString());
             Console.WriteLine(PC21.ToString());
 
             Console.ReadKey();
